b7956d486e95cdd01408c818ee26da7c2d0a315c
Andrew M Almeida <am.almeida000@gmail.com>
2022-08-22 22:24:34 -0700

    Time: 245 ms (36.08%), Space: 78.6 MB (40.37%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let valuesFound = [];
+    while (head) {
+        valuesFound.push(head.val);
+        head = head.next;
+    }
+
+    // Check if the list of values are a palindrome
+    let left = 0;
+    let right = valuesFound.length - 1;
+    while (left <= right) {
+        if (valuesFound[left] !== valuesFound[right]) {
+            return false;
+        }
+        left++, right--;
+    }
+
+    return true;
+};
