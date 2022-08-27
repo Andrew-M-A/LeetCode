03c77daadbc736a29c66c2642ee4523cca6e7bca
Andrew M Almeida <am.almeida000@gmail.com>
2022-08-27 13:19:31 -0700

    Time: 372 ms (31.02%), Space: 51.2 MB (50.01%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if (n === 1) {
+    return true;
+    } 
+    else if (n<1) {
+    return false;
+    }
+    let i = 1;
+    while(i < n){
+    i=i*3;
+    }
+return i === n;
+};
