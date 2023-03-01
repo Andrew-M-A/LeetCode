56f01d14851e1d600109ef6b36f587cb0b2c9d47
Andrew M Almeida <am.almeida000@gmail.com>
2023-02-28 23:04:19 -0800

    Time: 67 ms (57.66%), Space: 41.9 MB (90.73%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,8 +3,11 @@
  * @return {number[]}
  */
 
+// define a variable with a value of 0 to keep track of the previous value
 // loop through the array
-// set index[i]to the value of the first index (0) + i
+// set index[i]to the value of the variable defined in step 1 + nums[i]
+// re-assign nums[i] to sum
+// return nums from the function
 
 var runningSum = function(nums) {
     
