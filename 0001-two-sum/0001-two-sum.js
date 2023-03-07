271ea7b2071c2f65c3f3a7ca757f6b26fdfc1dd5
Andrew M Almeida <am.almeida000@gmail.com>
2023-03-07 15:09:07 -0800

    Time: 64 ms (90.00%), Space: 43.7 MB (14.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,21 @@
  * @return {number[]}
  */
 
+// use a hashmap to make referencing an index(key) and it's value easier
+// loop through the nums array
+// for each index, check if the hash table already has a value of target - nums[i]
+// if it does, return the current index and the index where target - nums[i] is
+// else, set the value of nums[i] into the hash
+// return an empty array
+
 var twoSum = function(nums, target) {
     let map1 = new Map();
-
-    for(let i = 0; i < nums.length; i++){
-        if(map1.has(target - nums[i])) {
-            return [map1.get(target - nums[i]), i]
+    for(let i = 0; i < nums.length; i++) {
+        if(map1.has(target-nums[i])) {
+            return [map1.get(target-nums[i]), i]
         } else {
-        map1.set(nums[i], i);
-      }
-}
-    return []
+            map1.set(nums[i], i)
+        }
+    }
+    return [];
 };
